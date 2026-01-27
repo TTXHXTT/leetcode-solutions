bc2640306ba1ed06568a3295ac6f3631b65037ab
Xiangyu <quxiangyu62@gmail.com>
2026-01-27 02:37:13 -0800

    Update 217.Contains Duplicate.java

--- 217.Contains Duplicate.java
+++ 217.Contains Duplicate.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> seen= new HashSet<>();
-
+        
+        //从 nums 数组里一个一个拿出数字
         for(int num:nums){
             if(seen.contains(num)){
                 return true;
