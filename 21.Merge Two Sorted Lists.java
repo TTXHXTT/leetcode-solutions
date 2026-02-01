626172199313dd7d84b9b5ac6e154ef08c231eb6
Xiangyu <quxiangyu62@gmail.com>
2026-02-01 04:28:24 -0800

    Create 21.Merge Two Sorted Lists.java

---
+++ 21.Merge Two Sorted Lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //用来存储最初的头
+        ListNode res1 = new ListNode();
+        //用来编辑链表
+        ListNode res = res1;
+
+
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                res.next=list1;
+                list1=list1.next;
+
+                }
+            else{
+                res.next=list2;
+                list2=list2.next;
+                }
+            res=res.next;
+        }
+        //跳出循环后代表一定有一个表空了
+        res.next=(list1!=null) ? list1 : list2;
+        //返回头的下一个，就是第一个值
+        return res1.next;
+        
+    }
+}
