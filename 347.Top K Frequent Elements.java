79b49a3fea1b7f570de3c4f56c013686f2a324c3
Xiangyu <quxiangyu62@gmail.com>
2026-02-05 07:59:48 -0800

    Create 347.Top K Frequent Elements.java

---
+++ 347.Top K Frequent Elements.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        //统计出现次数
+        HashMap<Integer,Integer> frep = new HashMap<>();
+        //遍历数组并存入
+        for(int x : nums) frep.put(x,frep.getOrDefault(x,0)+1);
+
+        //创建最小堆，用于存储结果
+        //(a,b) -> a[1] - b[1]   告诉堆如何比较，即比较数组中第二个元素
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+
+
+        //HashMap 是 Map 的一种实现，
+        //Map.Entry 是 Map 里的一条“键值对”。
+        //这是里面的一条 (key, value)
+
+        //一个 Map 有三种常用遍历方式：
+        //keySet()	所有 key
+        //values()	所有 value
+        //entrySet()	所有 (key, value) 对
+        for(Map.Entry<Integer,Integer> e : frep.entrySet()){
+            //将得到的数据压入堆中
+            heap.offer(new int[]{e.getKey(),e.getValue()});
+            //保持堆为k个元素，超出poll掉最小的堆顶
+            if(heap.size()>k){
+                heap.poll();
+            }
+        }
+        //将最终得到的最小堆的没一个数组第一个元素（即nums中出现的元素），存入最终结果数组中
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=heap.poll()[0];      
+        }
+        return res;
+    }
+}
