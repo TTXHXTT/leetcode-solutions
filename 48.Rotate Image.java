17b31ab900a7c1b7bed80895d6b32915e7a9962c
Xiangyu <quxiangyu62@gmail.com>
2026-02-08 08:37:54 -0800

    Create 48.Rotate Image.java

---
+++ 48.Rotate Image.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        /* 
+         * 第一步：沿主对角线进行转置
+         * 作用：把 matrix[i][j] 和 matrix[j][i] 交换
+         * 注意：j 从 i+1 开始，避免重复交换和交换自己
+         */
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]= temp;
+            }
+        }
+
+        /*
+         * 第二步：对每一行进行左右翻转
+         * 作用：完成顺时针 90° 旋转
+         */
+        for(int i=0;i<n;i++){
+            
+            int right=n-1,left=0;
+             // 双指针向中间收缩，交换左右元素
+            while(right>left){
+                int temp = matrix[i][right];
+                matrix[i][right]=matrix[i][left];
+                matrix[i][left]= temp;
+                right--;
+                left++;
+            }
+        }
+    }
+}
