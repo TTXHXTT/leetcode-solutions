27c0d2ef99e66ef8c5453bb2f8cdd2eedc20b6b6
Xiangyu <quxiangyu62@gmail.com>
2026-02-12 09:55:50 -0800

    Create 205.Isomorphic Strings.java

---
+++ 205.Isomorphic Strings.java
@@ -0,0 +1,52 @@
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        //使用ASCII来代替hashmap
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+
+            //曾经出现过已经建立关系的字符，判断是否能映射回去
+            if(mapS[c1]!=mapT[c2]) return false;
+            
+            //比较的是“出现模式是否一致”
+            mapS[c1] = i + 1;
+            mapT[c2] = i + 1;         
+        }
+
+
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        HashMap<Character,Character> mapa=new HashMap<>();
+        HashMap<Character,Character> mapb=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+
+            // 如果之前已经建立映射
+            if(mapa.containsKey(c1)){
+                if(mapa.get(c1)!=c2)  return false;
+            }else{
+                //如果c1没出现再1号map，然而c2出现在2号map
+                //那说明c2被其他人映射
+                if(mapb.containsKey(c2)) return false;
+            }
+
+            mapa.put(c1,c2);
+            mapb.put(c2,c1);
+        }
+
+        return true;
+    }
+}
