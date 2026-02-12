b64658d63473763c72199a312dae74fca198e9dc
Xiangyu <quxiangyu62@gmail.com>
2026-02-12 09:36:01 -0800

    Create 58.Length of Last Word.java

---
+++ 58.Length of Last Word.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n=s.length();
+        int i = s.length()-1;
+       
+        // 跳过字符串末尾的所有空格
+        while(i>=0&&s.charAt(i)==' ') i--;
+        
+        //j 记录最后一个单词的“末尾位置”
+        int j=i;
+       
+        //继续往前扫描，直到遇到空格
+        while(i>=0&&s.charAt(i)!=' ') i--;
+        
+        // substring 左闭右开
+        // 单词区间是 (i+1) 到 j（包含 j）
+        // 所以写成 substring(i+1, j+1)
+        String res=s.substring(i+1,j+1);
+        
+        
+        return res.length();
+        
+    }
+}Q
