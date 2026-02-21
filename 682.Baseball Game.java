5fa0e54708ca052441f8bef736db1d19e11d6323
Xiangyu <quxiangyu62@gmail.com>
2026-02-20 20:50:54 -0800

    Create 682.Baseball Game.java

---
+++ 682.Baseball Game.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        // 用栈来存储每一轮有效的得分
+        Stack<Integer> stack = new Stack<>();
+
+        // 遍历所有操作
+        for (String op : operations) {
+
+            // 情况1：当前得分 = 前两个得分之和
+            if (op.equals("+")) {
+                // 先取出栈顶元素
+                int top = stack.pop();
+                // 新得分 = 栈顶 + 次栈顶
+                int newScore = top + stack.peek();
+                // 把刚刚弹出的元素放回去
+                stack.push(top);
+                // 把新得分入栈
+                stack.push(newScore);
+
+            // 情况2：当前得分 = 前一个得分的两倍
+            } else if (op.equals("D")) {
+                stack.push(stack.peek() * 2);
+
+            // 情况3：删除前一个得分
+            } else if (op.equals("C")) {
+                stack.pop();
+
+            // 情况4：普通数字，直接入栈
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        // 计算最终总和
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
