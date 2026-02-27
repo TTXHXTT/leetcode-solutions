a099b43f02d3838d55da5c6161bf99000d4c3dd9
Xiangyu <quxiangyu62@gmail.com>
2026-02-27 03:59:44 -0800

    Update 217.Contains Duplicate.java

--- 217.Contains Duplicate.java
+++ 217.Contains Duplicate.java
@@ -1,8 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        
+        第一步：先说整体思路
+        About this problem I use a HashSet to store the number we seen.
+        I loop through the array.
+        For each number, I check if it is already in the set.
+        If yes, I return true. If it’s not in the set, I add it into the set.
+        If we finish the loop and never find duplicates, we return false.
+        Time complexity is O(n),because we only go through the array once.
+        Space complexity is O(n).
+
+            
         HashSet<Integer> seen= new HashSet<>();
         
-        //从 nums 数组里一个一个拿出数字
+        //从 nums 数组里一个一个拿出数字 
         for(int num:nums){
             if(seen.contains(num)){
                 return true;
