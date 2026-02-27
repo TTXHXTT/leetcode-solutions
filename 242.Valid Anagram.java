b8078db6f647eefd4ef89e553228c50fb618a84a
Xiangyu <quxiangyu62@gmail.com>
2026-02-27 04:20:06 -0800

    Update 242.Valid Anagram.java

--- 242.Valid Anagram.java
+++ 242.Valid Anagram.java
@@ -1,10 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        For this problem, I use a HashMap to store the character counts.
+        First, I check if the two strings have different lengths. If they do, I return false.
+        Then I loop through the first string. I count how many times each character appears in String.
+        After that, I loop through the second string.
+        For each character in t, I check if it exists in the map and if its count is greater than zero.
+        If not, that means the strings are not anagrams. So I return false. 
+        If it exists, I decrease its count by one.
+        If we finish the loop I return true.
+        Time complexity is O(n), because we go through both strings once.
+        Space complexity is O(n).
+
+            
         if (s.length() != t.length()) {
            return false;
         }
 
-        
         HashMap<Character , Integer> counter =  new  HashMap<>();
 
         for (int i=0;i<s.length();i++){
