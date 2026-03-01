7706ca108cd0b7a18235d2fde811cbda82825dd0
Xiangyu <quxiangyu62@gmail.com>
2026-03-01 07:24:16 -0800

    Update 844.Backspace String Compare.java

--- 844.Backspace String Compare.java
+++ 844.Backspace String Compare.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+
+        I use two stacks for two String.
+        First, I go through string s.
+            If the character is '#', it means backspace.
+            So I remove the top character from the stack, if the stack is not empty.
+            Otherwise, I push the character onto the stack.
+        I apply the same logic to string t.
+
+        Finally, I compare the two stacks.
+            If they are equal, it means they are the same.
+
+        Time complexity is O(n + m), where n and m are the lengths of the two strings.
+        Space complexity is O(n + m), because we may store all characters in the stacks.
+            
+        
         Stack<Character> st1 = new Stack<>();
         Stack<Character> st2 = new Stack<>();
 
