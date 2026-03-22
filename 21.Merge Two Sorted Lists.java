e0dbe46f0bf28eadf9a7fe8af491757b10620dd3
Xiangyu <quxiangyu62@gmail.com>
2026-03-22 02:47:26 -0700

    Update 21.Merge Two Sorted Lists.java

--- 21.Merge Two Sorted Lists.java
+++ 21.Merge Two Sorted Lists.java
@@ -1,10 +1,21 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+I use a dummy node to build the result list.
+
+I go through both lists at the same time.
+Each time, I compare the values and attach the smaller one.
+
+After the loop, I attach the remaining nodes.
+
+Finally, I return the merged list.
+
+Time is O(n + m), space is O(1).
+        
         //用来存储最初的头
         ListNode res1 = new ListNode();
         //用来编辑链表
         ListNode res = res1;
-
 
         while(list1!=null && list2!=null){
             if(list1.val<=list2.val){
