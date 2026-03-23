de7f09857c0efa8cc77d210b0f23416c70a16082
Xiangyu <quxiangyu62@gmail.com>
2026-03-23 01:21:49 -0700

    Update 704.Binary Search.java

--- 704.Binary Search.java
+++ 704.Binary Search.java
@@ -1,4 +1,17 @@
 class Solution {
+
+I use binary search with recursion.
+
+Each time, I check the middle element.
+If it equals the target, I return the index.
+
+If the target is smaller, I search the left half.
+Otherwise, I search the right half.
+
+If the range becomes invalid, I return -1.
+
+Time is O(log n), space is O(log n).
+    
     public int search(int[] nums, int target) {
         return s(target,0,nums.length-1,nums);
 
@@ -10,7 +23,7 @@ class Solution {
         
         if(nums[mid]==target) return mid;
 
-        if(target<=nums[mid]){
+        if(target<nums[mid]){
             j=mid-1;
             return s(target,i,j,nums);
         }else{
