5d857bec00339b88ded229c1182205f6ade083ab
Xiangyu <quxiangyu62@gmail.com>
2026-03-24 02:19:59 -0700

    Update 70.Climbing Stairs.java

--- 70.Climbing Stairs.java
+++ 70.Climbing Stairs.java
@@ -1,5 +1,17 @@
 class Solution {
 
+This is a dynamic programming problem.
+
+I use memoization to avoid repeated compute.
+
+To reach step n,
+we can come from step n-1 or n-2.
+
+So the result is the sum of the previous two.
+
+Time is O(n), space is O(n).
+
+    
 //本质上与斐波那契数列是一样的
 //每次走一步或者两步，那么  Res-> n = n-1  +  n-2;不断递归
     int[] memo;
