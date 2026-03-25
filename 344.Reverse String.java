6401d0eab0b02c0eb33363e495f40c349fdaf1da
Xiangyu <quxiangyu62@gmail.com>
2026-03-25 00:20:28 -0700

    Update 344.Reverse String.java

--- 344.Reverse String.java
+++ 344.Reverse String.java
@@ -1,5 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
+
+I use two pointers,
+one from the left and one from the right.
+
+I swap the characters
+and move both pointers toward the center.
+
+Time is O(n), space is O(1).
+
         int i=0,j=s.length-1;
         while(i<j){
             char a = s[i];
