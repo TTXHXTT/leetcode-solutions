27c9a7b9985d0890231b1263fe7233b5747f801e
Xiangyu <quxiangyu62@gmail.com>
2026-03-25 00:46:59 -0700

    Update 26.Remove Duplicates from Sorted Array.java

--- 26.Remove Duplicates from Sorted Array.java
+++ 26.Remove Duplicates from Sorted Array.java
@@ -1,5 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+
+I use a pointer to track the position of unique elements.
+
+I go through the array.
+If the current element is different from the last unique element,
+I move it forward.
+
+At the end, I return the pointer position.
+            
         int  j=1;
         for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[i-1]){
