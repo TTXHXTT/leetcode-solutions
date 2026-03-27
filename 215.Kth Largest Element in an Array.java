b1ded304f2c48fb9e7c2af1151da5502e3069d99
Xiangyu <quxiangyu62@gmail.com>
2026-03-27 02:06:11 -0700

    Update 215.Kth Largest Element in an Array.java

--- 215.Kth Largest Element in an Array.java
+++ 215.Kth Largest Element in an Array.java
@@ -1,5 +1,17 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+
+I use a min heap of size k.
+
+I go through the array and add each element to the heap.
+If the size exceeds k, I remove the smallest element.
+
+This way, the heap always contains the k largest elements.
+
+At the end, the top of the heap is the k-th largest.
+
+Time is O(n log k), space is O(k).
+    
         //这是一个默认完整的二叉树，小顶堆
         PriorityQueue<Integer> heap = new PriorityQueue<>();
 
