277a517c8ad0d1d79c7c84de729f2832b04d2706
Xiangyu <quxiangyu62@gmail.com>
2026-04-05 02:47:02 -0700

    Update 496.Next Greater Element l.java

--- 496.Next Greater Element l.java
+++ 496.Next Greater Element l.java
@@ -1,6 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
+I use a monotonic decreasing stack and a HashMap.
+
+First, I go through nums2.
+If the current number is greater than the stack top,
+then it is the next greater element for that number.
+So I pop it and store the mapping.
+
+Then I push the current number into the stack.
+
+After that,
+any number left in the stack does not have a next greater element,
+so I map it to -1.
+
+Finally, I build the result for nums1 using the map.
+
+Time is O(n), space is O(n).
+        
+
         // 存每个元素的下一个更大元素
         HashMap<Integer, Integer> map = new HashMap<>();
 
